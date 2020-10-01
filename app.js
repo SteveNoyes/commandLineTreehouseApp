0c4cc7accb12a6bacf3272db09f214dadd25fcf4
SteveNoyes <stevenmnoyes.af@gmail.com>
2020-10-01 09:51:18 -0700

    end

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const https = require('https');
-const username = "stevennoyes";
+const profile = require('./profile.js');
 
-function printMessage(username, badgeCount, point) {
-  const message = `${username} has ${badgeCount} total badge(s) and ${point} points in JavaScript.`;
-  console.log(message);
-}
-
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-	let body = '';			
-	response.on('data', data => {
-  	body += data.toString();
-	});
-
-	response.on('end', () => {
-	  const profile = JSON.parse(body);
-	  
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
-					
-	});
-});
+const users = process.argv.slice(2);
+users.forEach(profile.get);
